7f9c6f762a5b527c9ce87fc6df7c40c1a6c65767
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-09-12 12:58:30 -0400

    updated events handler

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Calendar from 'react-calendar';
 
 import EventForm from './components/EventForm';
 import 'react-calendar/dist/Calendar.css';
+import './Calendar.css';
 import './App.css';
 
 const eventsData = [
@@ -15,20 +16,25 @@ const eventsData = [
 ];
 
 function App() {
+  const [isDateSelected, setIsDateSelected] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [events, setEvents] = useState(eventsData);
   const [calendarDay, setCalendarDay] = useState(new Date());
 
-  const daysWithEvents = [];
   let eventsDays = eventsData.map((eventDay) => eventDay.date);
-  console.log(eventsDays);
 
-  const addEvent = (date) => {
+  const selectDateHandler = (date) => {
     setCalendarDay(date);
+    setIsDateSelected(true);
+  }
+
+  const addEventHandler = (date) => {
+    setIsDateSelected(false);
     setIsEditing(true);
   }
 
-  const cancelEvent = () => {
+  const cancelEventHandler = () => {
+    setIsDateSelected(false)
     setIsEditing(false);
   }
 
@@ -39,17 +45,13 @@ function App() {
       date: calendarDay.toDateString()
     }
     setEvents(eventsData.push(eventData));
-    //console.log(eventData);
-    
-    // daysWithEvents.push(eventsData.date);
-    // console.log(daysWithEvents);
   }
 
   const filteredEvents = eventsData.filter(element => {
     return element.date === calendarDay.toDateString();
   });
 
-  let event = 
+  let eventList = filteredEvents.length !== 0 ?
     <div className='events-container'>
       {filteredEvents.map((item) => (
         <div className='event'  key={item.id}>
@@ -57,30 +59,30 @@ function App() {
           <p>{item.notes}</p>
         </div>
       ))}
-    </div>;
+    </div> :
+    <h3 className='events-empty'>No Found Events</h3>;
 
   const hasEventsHandler = ({ date, view }) => {
-    // console.log(calendarDay.getDate());
-    // return date.getDate() === calendarDay.getDate() ? <div className="event-indicator"></div> : null;
-
-    // console.log(date.toDateString());
-    // console.log(daysWithEvents);
     if (view === 'month' && checkEventsDate(date)) {
       return <div className="event-indicator"></div>
     }
   };
 
   function checkEventsDate(date) {
-    // return daysWithEvents.some(event => event.toDateString() === date.toDateString());
     return eventsDays.some(event => event === date.toDateString());
   }
 
   return (
     <div className='app-container'>
       <div className='header-container'></div>
-      <Calendar onClickDay={(date) => addEvent(date)} tileContent={( date, view ) => hasEventsHandler(date, view)} />
-      {isEditing && <EventForm onCancel={cancelEvent} onSaveEvent={onSaveDataHandler}/>}
-      {isEditing && event}
+      <Calendar onClickDay={(date) => selectDateHandler(date)} tileContent={( date, view ) => hasEventsHandler(date, view)} />
+      {isDateSelected && 
+      <div className='add-new-event__container'>
+        <button onClick={cancelEventHandler}>Back</button>
+        <button onClick={addEventHandler}>Add New Event</button>
+      </div>}
+      {isEditing && <EventForm onCancel={cancelEventHandler} onSaveEvent={onSaveDataHandler}/>}
+      {(isDateSelected || isEditing)  && eventList}
     </div>
   );
 }
