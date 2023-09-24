05c16d0d793d1e70a191b2c8d6b19fb5d83553a3
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-09-24 00:13:41 -0400

    added focus trapping to modal component

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import classes from './Modal.module.css';
@@ -10,6 +10,32 @@ const Backdrop = () => {
 const portalElement = document.getElementById('overlays');
 
 const Modal = (props) => {
+  useEffect(() => {
+    const keyDownHandler = (event) => {
+      if (event.key === "Tab") {
+        const focusableElements = portalElement.querySelectorAll(
+          "button, [href], input, select, textarea, [tabindex]:not([tabindex='-1'])"
+        );
+        const firstElement = focusableElements[0];
+        const lastElement = focusableElements[focusableElements.length - 1];
+  
+        if (event.shiftKey && document.activeElement === firstElement) {
+          lastElement.focus();
+          event.preventDefault();
+        } else if (!event.shiftKey && document.activeElement === lastElement) {
+          firstElement.focus();
+          event.preventDefault();
+        }
+      }
+    };
+  
+    document.addEventListener("keydown", keyDownHandler);
+  
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
+  }, []);  
+
   return <Fragment>
     {ReactDOM.createPortal(<Backdrop/>, portalElement)}
     {ReactDOM.createPortal(<div className={classes.modal}>{props.children}</div>, portalElement)}
